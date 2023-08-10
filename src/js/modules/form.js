ade0c4aac097e69c30b7de817ed5d0f7d83bceb9
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2023-08-10 13:04:23 +0500

    upd form js

--- src/js/modules/form.js
+++ src/js/modules/form.js
@@ -139,7 +139,7 @@ $$("form").forEach((form) => {
 			params.append(pair[0], pair[1]);
 		}
 		// await fetch('https://alexsab.ru/lead/test/', {
-		await fetch("https://alexsab.ru/lead/jetour/alpha/", {
+		await fetch("https://alexsab.ru/lead/jac-samara/", {
 			method: "POST",
 			mode: "cors",
 			cache: "no-cache",
