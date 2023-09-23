074d4bcec4e56378ae49cac62ebddd9408e5c85e
Alexander Sabanov <alexsab@mail.ru>
2023-09-23 11:13:52 +0400

    upd disclamer, add btn-up, cookie, analytics

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export const theme = {
   },
   colors: {
     blue: '#1b3252',
-    red: colors.red,
+    red: '#C50B0D',
     accent: '#C50B0D',
     transparent: 'transparent',
     white: colors.white,
