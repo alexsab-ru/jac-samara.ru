165ce3e4f6ecee3eda913460685f33ad3758c6fe
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2023-10-12 19:45:46 +0500

    Update scroll.js

--- src/js/modules/scroll.js
+++ src/js/modules/scroll.js
@@ -3,8 +3,8 @@ window.location.hash = '';
 
 if(hashURL){
 	window.addEventListener("load", function (event) {
-		setTimeout(() => scroll(hashURL.substring(1)), 1000)
-		window.location.hash = hashURL;
+		setTimeout(() => scroll(hashURL.substring(1)), 200)
+		// window.location.hash = hashURL;
 	});
 }
 
