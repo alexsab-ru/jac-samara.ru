a04380115827d6a589e3715732ccec54a3eb5cc1
Alexander Sabanov <alexsab@mail.ru>
2024-06-17 17:58:34 +0400

    upd cars color

--- .github/scripts/config.py
+++ .github/scripts/config.py
@@ -1,8 +1,29 @@
 dealer = {
-    "city": "Оренбург",
-    "where": "Оренбурге",
+    "city": "Самара",
+    "where": "Самаре",
 }
 # Словарь соответствия цветов
 model_mapping = {
+    "J7, I": {
+        "folder": "j7",
+        "color": {
+            "Черный": "black.webp",
+            "Черный/черный": "black.webp",
+            "Синий": "blue.webp",
+            "Серый": "gray.webp",
+            "Серый металлик": "gray.webp",
+            "Красный": "red.webp",
+            "Белый": "white.webp",
+        }},
+    "JS3": {
+        "folder": "js3",
+        "color": {
+            "Черный": "black.webp",
+            "Черный/черный": "black.webp",
+            "Серый": "gray.webp",
+            "Серый металлик": "gray.webp",
+            "Серебристый": "silver.webp",
+            "Белый": "white.webp",
+        }},
    # ... добавьте другие модели по мере необходимости
 }
