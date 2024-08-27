0e4956df55855d71d5b733496807cab465f795f7
Alexander Sabanov <alexsab@mail.ru>
2024-08-27 15:44:58 +0400

    upd config.py

--- .github/scripts/config.py
+++ .github/scripts/config.py
@@ -10,6 +10,7 @@ model_mapping = {
             "Черный": "black.webp",
             "Черный/черный": "black.webp",
             "Синий": "blue.webp",
+            "Темно-серый": "gray.webp",
             "Серый": "gray.webp",
             "Серый металлик": "gray.webp",
             "Красный": "red.webp",
@@ -20,8 +21,47 @@ model_mapping = {
         "color": {
             "Черный": "black.webp",
             "Черный/черный": "black.webp",
+            "Серебристо-темно-фиолето": "gray.webp",
             "Серый": "gray.webp",
             "Серый металлик": "gray.webp",
+            "Серебристый": "silver.webp",
+            "Белый": "white.webp",
+        }},
+    "JS6": {
+        "folder": "js6",
+        "color": {
+            "Черный": "black.webp",
+            "Коричневый": "brown.webp",
+            "Серый": "gray.webp",
+            "Красный": "red.webp",
+            "Белый": "white.webp",
+        }},
+    "T6": {
+        "folder": "t6",
+        "color": {
+            "Черный": "black.webp",
+            "Синий": "blue.webp",
+            "Красный": "red.webp",
+            "Серебристый": "silver.webp",
+            "Серебристо-темно-фиолето": "silver.webp",
+            "Белый": "white.webp",
+        }},
+    "T8": {
+        "folder": "t8-pro",
+        "color": {
+            "Белый": "white.webp",
+            "Серый": "gray.webp",
+            "Черный": "black.webp",
+            "Красный": "red.webp",
+            "Синий": "blue.webp",
+        }},
+    "T9": {
+        "folder": "t9",
+        "color": {
+            "Черный": "black.webp",
+            "Синий": "blue.webp",
+            "Серый": "gray.webp",
+            "Красный": "red.webp",
             "Серебристый": "silver.webp",
             "Белый": "white.webp",
         }},
